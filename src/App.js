313fd577d1a85bed32b3cbe748b07e90e7222794
Meka-tech <nnaemekaonyeji27@gmail.com>
2022-07-13 14:14:04 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./Componets/Button/Button";
+import Form from "./Componets/Form/Form";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form title="First Name" placeholder="First Name" type="text" />
+
+      <Button />
     </div>
   );
 }
---
+++ src/Componets/Button/Button.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = ({
+  text = "Button",
+  bgColor,
+  fontSize = "16px",
+  fontWeight,
+  borderRadius,
+  color,
+  border,
+}) => {
+  return (
+    <Container
+      bgColor={bgColor}
+      fontSize={fontSize}
+      borderRadius={borderRadius}
+      color={color}
+      border={border}
+    >
+      {text}
+    </Container>
+  );
+};
+
+const Container = styled.button`
+  outline: none;
+  border: none;
+  cursor: pointer;
+  padding: 10px 20px;
+  width: fit-content;
+  border-radius: ${(props) =>
+    props.borderRadius ? props.borderRadius : "5px"};
+  color: ${(props) => (props.color ? props.color : "white")};
+  border: ${(props) => (props.border ? props.border : "none")};
+  background-color: ${(props) =>
+    props.bgColor ? props.bgColor : "rgba(0, 0, 255, 0.9)"};
+
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "16px")};
+  font-weight: 600;
+  line-height: 20px;
+  letter-spacing: 0em;
+  text-align: left;
+`;
+
+export default Button;
